479661f409a9558b4e0bf7d3b990cd8781be8b7a
yugasamuel <128349047+yugasamuel@users.noreply.github.com>
2023-05-25 12:55:16 +0700

    Add files via upload

---
+++ Ascend!/Ascend!UITests/Ascend_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ascend_UITestsLaunchTests.swift
+//  Ascend!UITests
+//
+//  Created by Yuga Samuel on 18/05/23.
+//
+
+import XCTest
+
+final class Ascend_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
